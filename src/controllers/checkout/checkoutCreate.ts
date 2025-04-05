7e1f3bbcf8d62e1c88d2dc40dc614f3a5766bb2e
Iván Ruiz López <ivruizl@alumnos.unex.es>
2025-04-05 16:47:41 +0200

    Auto redirect to payment page

--- src/controllers/checkout/checkoutCreate.ts
+++ src/controllers/checkout/checkoutCreate.ts
@@ -9,7 +9,7 @@ interface CartItemProps {
 }
 
 export const checkoutCreate = async (req: express.Request, res: express.Response) => {
-    if (!req.body.cart) {
+    if (!req.body.cart || req.body.cart.length == 0) {
         res.status(200).send({err: 'INVALID_CART'})
         return 
     }
@@ -22,21 +22,25 @@ export const checkoutCreate = async (req: express.Request, res: express.Response
                 product_data: {
                     name: item.id,
                 },
-                unit_amount: 6.99,
+                unit_amount: 600, // En céntimos
             },
             quantity: item.quantity,
         }
     ))
+    console.log(lineItems)
 
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '')
-    const session = await stripe.checkout.sessions.create({
-        success_url: `http://${process.env.APP_URL}:${process.env.PORT}/shop/checkout/success?checkoutSession={CHECKOUT_SESSION_ID}`,
-        cancel_url: `http://${process.env.APP_URL}:${process.env.PORT}/shop/checkout/fail?checkoutSession={CHECKOUT_SESSION_ID}`,
-        line_items: lineItems,
-        mode: 'payment'
-    })
-    // TODO Crear pedido en BD pendiente de pago, incluir sessionID
-
-    res.send(session)
-    // res.redirect(303, session.url);
+    try {
+        const session = await stripe.checkout.sessions.create({
+            success_url: `http://${process.env.APP_URL}:${process.env.PORT}/shop/checkout/success?checkoutSession={CHECKOUT_SESSION_ID}`,
+            cancel_url: `http://${process.env.APP_URL}:${process.env.PORT}/shop/checkout/fail?checkoutSession={CHECKOUT_SESSION_ID}`,
+            line_items: lineItems,
+            mode: 'payment',
+            payment_method_types: ['card']
+        })
+        // TODO Crear pedido en BD pendiente de pago, incluir sessionID
+        res.redirect(303, session.url!)
+    } catch (err) {
+        res.status(400).send({msg: 'ERR_CREATING_ORDER'})
+    }
 }
--- src/controllers/checkout/checkoutSuccess.ts
+++ src/controllers/checkout/checkoutSuccess.ts
@@ -3,17 +3,23 @@ import Stripe from 'stripe'
 
 export const checkoutSuccess = async (req: express.Request, res: express.Response) => {
     if (!req.body.sessionId) {
-        res.status(200).send({err: 'INVALID_SESSION_ID'})
+        res.status(200).send({ err: 'NO_SESSION_ID_PROVIDED' })
         return
     }
 
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '')
     const sessionId: string = req.body.sessionId
 
-    const session = await stripe.checkout.sessions.retrieve(sessionId)
-    if (session.payment_status == 'paid') {
-        // Buscar pedido asociado a ese ID, marcar como pagado
-    } else {
-        // Devolver error
+    try {
+        const session = await stripe.checkout.sessions.retrieve(sessionId)
+        if (session.payment_status == 'paid') {
+            // Buscar pedido asociado a ese ID, marcar como pagado
+            res.send({msg: 'ORDER_PAID'})
+        } else {
+            // Devolver error
+            res.send({msg: 'ORDER_NOT_PAID'})
+        }
+    } catch (err) {
+        res.status(400).send({msg: 'SESSION_ID_NOT_FOUND'})
     }
 }
