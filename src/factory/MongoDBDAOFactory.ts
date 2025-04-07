5d7474a3ca379d205f1ee4b0ed55fcb9f494470f
Iván Ruiz López <ivruizl@alumnos.unex.es>
2025-04-07 09:09:12 +0200

    Created DAO Factory structure

--- src/factory/MongoDBDAOFactory.ts
+++ src/factory/MongoDBDAOFactory.ts
@@ -1,5 +1,58 @@
+import mongoose from "mongoose";
 import { InterfaceDAOFactory } from "./InterfaceDAOFactory";
 
 export class MongoDBDAOFactory implements InterfaceDAOFactory {
+    constructor() {
+        try {
+            mongoose.connect(process.env.DB_URI!)
+        } catch (err) {
+            console.log('Connection to DB failed:')
+            console.log(err)
+        }
+    }
 
+    createBaseUserDAO(): BaseUserDAO {
+        
+    }
+
+    createUserDAO(): UserDAO {
+        
+    }
+
+    createArtistDAO(): ArtistDAO {
+        
+    }
+
+    createProductDAO(): ProductDAO {
+        
+    }
+
+    createSongDAO(): SongDAO {
+        
+    }
+
+    createAlbumDAO(): AlbumDAO {
+        
+    }
+
+    createGenreDAO(): GenreDAO {
+        
+    }
+
+    createRatingDAO(): RatingDAO {
+        
+    }
+
+    createOrderDAO(): OrderDAO {
+        
+    }
+
+    async closeConnection(): Promise<boolean> {
+        try {
+            await mongoose.disconnect()
+            return true
+        } catch (_err) {
+            return false
+        }
+    }
 }
