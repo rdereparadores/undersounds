6128da7c50b67e4a3445e61662e732d23dac0e7a
Alejandro Paniagua <apaniaguu@alumnos.unex.es>
2025-04-12 15:56:27 +0200

    Hecha funcionalidad de OTP, creación y verificación

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -4,9 +4,12 @@ import { authSignUpController } from '../controllers/auth/authSignUpController'
 import { authSignInController } from '../controllers/auth/authSignInController'
 import { authTokenMiddleware } from '../middleware/authTokenMiddleware'
 import { authSignUpGoogleController } from '../controllers/auth/authSignUpGoogleController'
+import { authConfirmOtpController, authSetOtpController } from '../controllers/auth/authOtpController'
 
 export const authRouter = express.Router()
 
+authRouter.post('/setotp', authSetOtpController)
+authRouter.post('/confirmotp', authConfirmOtpController)
 authRouter.post('/signup', authSignUpController)
 authRouter.post('/signupgoogle', authSignUpGoogleController)
 authRouter.post('/signin',authTokenMiddleware, authSignInController)
