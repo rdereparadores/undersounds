d103e0648af6ee1f2f2d62d2f922c79f41c9773d
edgomeza <edgomeza@alumons.unex.es>
2025-04-18 11:43:19 +0200

    Añadida la documentación de las rutas revisadas de canción.

--- src/routes/songRouter.ts
+++ src/routes/songRouter.ts
@@ -5,5 +5,165 @@ import { songRatingController } from "../controllers/song/songRatingController"
 export const songRouter = express.Router()
 
 // REVISADAS
+
+/**
+ * @swagger
+ * /song/info:
+ *  post:
+ *      tags:
+ *          - Canciones
+ *      summary: Obtiene información detallada de una canción
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      required:
+ *                          - songId
+ *                      properties:
+ *                          songId:
+ *                              type: string
+ *                              description: ID de la canción
+ *      responses:
+ *          '200':
+ *              description: Información de la canción obtenida con éxito
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              data:
+ *                                  type: object
+ *                                  properties:
+ *                                      song:
+ *                                          type: object
+ *                                          properties:
+ *                                              _id:
+ *                                                  type: string
+ *                                                  description: ID de la canción
+ *                                              title:
+ *                                                  type: string
+ *                                                  description: Título de la canción
+ *                                              description:
+ *                                                  type: string
+ *                                                  description: Descripción de la canción
+ *                                              imgUrl:
+ *                                                  type: string
+ *                                                  description: URL de la portada
+ *                                              author:
+ *                                                  type: string
+ *                                                  description: ID del autor
+ *                                              duration:
+ *                                                  type: number
+ *                                                  description: Duración en segundos
+ *                                              genres:
+ *                                                  type: array
+ *                                                  items:
+ *                                                      type: string
+ *                                                  description: IDs de los géneros
+ *                                      recommendations:
+ *                                          type: array
+ *                                          items:
+ *                                              type: object
+ *                                              properties:
+ *                                                  _id:
+ *                                                      type: string
+ *                                                      description: ID de la canción recomendada
+ *                                                  title:
+ *                                                      type: string
+ *                                                      description: Título de la canción recomendada
+ *                                                  imgUrl:
+ *                                                      type: string
+ *                                                      description: URL de la portada
+ *          '400':
+ *              description: Error en la solicitud
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/ErrorResponse'
+ *          '500':
+ *              description: Error del servidor
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/ErrorResponse'
+ *      security: []
+ */
 songRouter.post('/info', songInfoController)
+
+/**
+ * @swagger
+ * /song/ratings:
+ *  post:
+ *      tags:
+ *          - Canciones
+ *      summary: Obtiene las valoraciones de una canción
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      required:
+ *                          - songId
+ *                      properties:
+ *                          songId:
+ *                              type: string
+ *                              description: ID de la canción
+ *      responses:
+ *          '200':
+ *              description: Valoraciones obtenidas con éxito
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              data:
+ *                                  type: object
+ *                                  properties:
+ *                                      ratings:
+ *                                          type: array
+ *                                          items:
+ *                                              type: object
+ *                                              properties:
+ *                                                  _id:
+ *                                                      type: string
+ *                                                      description: ID de la valoración
+ *                                                  rating:
+ *                                                      type: number
+ *                                                      description: Puntuación (1-5)
+ *                                                  title:
+ *                                                      type: string
+ *                                                      description: Título de la valoración
+ *                                                  description:
+ *                                                      type: string
+ *                                                      description: Comentario de la valoración
+ *                                                  author:
+ *                                                      type: string
+ *                                                      description: ID del autor de la valoración
+ *                                                  publishDate:
+ *                                                      type: string
+ *                                                      format: date
+ *                                                      description: Fecha de publicación
+ *                                      averageRating:
+ *                                          type: number
+ *                                          description: Puntuación media
+ *                                      totalRatings:
+ *                                          type: number
+ *                                          description: Número total de valoraciones
+ *          '400':
+ *              description: Error en la solicitud
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/ErrorResponse'
+ *          '500':
+ *              description: Error del servidor
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/ErrorResponse'
+ *      security: []
+ */
 songRouter.post('/ratings', songRatingController)
