25518ff247ca0fb644387cd0aa6b23774cb41651
ManahenG <mgarciacjg@alumnos.unex.es>
2025-04-23 13:42:08 +0200

    cambios oportunos para ajustar funcionalidad de valoraciones

--- src/controllers/artist/artistTransitionsController.ts
+++ src/controllers/artist/artistTransitionsController.ts
@@ -1,4 +1,3 @@
-// src/controllers/artistTransactionsController.ts
 import express from 'express'
 import apiErrorCodes from '../../utils/apiErrorCodes.json'
 
@@ -7,7 +6,6 @@ export const artistTransactionsController = async (req: express.Request, res: ex
         const artistDAO = req.db!.createArtistDAO()
         const orderDAO  = req.db!.createOrderDAO()
 
-        // 1) Buscamos el artista por su uid (lo inyectas en req.uid en tu auth middleware)
         const artist = await artistDAO.findByUid(req.uid!)
         if (!artist) {
             return res.status(404).json({
@@ -15,13 +13,11 @@ export const artistTransactionsController = async (req: express.Request, res: ex
             })
         }
 
-        // 2) Traemos todas las líneas de pedido que contengan productos de este artista
         const itemsSold = await orderDAO.findItemsSoldByArtist(artist)
         if (!itemsSold) {
             return res.json({ data: [] })
         }
 
-        // 3) Convertimos cada unidad vendida en un objeto Transaction
         const transactions = itemsSold.flatMap(line =>
             Array.from({ length: line.quantity }, (_, i) => {
                 const date    = new Date(line.purchaseDate)
@@ -40,10 +36,8 @@ export const artistTransactionsController = async (req: express.Request, res: ex
             })
         )
 
-        // 4) Ordenamos por fecha, más recientes primero
         transactions.sort((a, b) => b.date.getTime() - a.date.getTime())
 
-        // 5) Devolvemos la lista completa
         return res.json({ data: transactions })
 
     } catch (err) {
