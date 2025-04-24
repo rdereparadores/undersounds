79f34bd771b5c8e33a1ac760df944264a3f612fc
Alejandro Paniagua <apaniaguu@alumnos.unex.es>
2025-04-24 18:32:31 +0200

    it returns the actual version too

--- src/controllers/artist/artistSongsHistoryController.ts
+++ src/controllers/artist/artistSongsHistoryController.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import apiErrorCodes from '../../utils/apiErrorCodes.json'
+import { SongDAO } from '../../dao/SongDAO'
 
 export const artistSongsHistoryController = async (req: express.Request, res: express.Response) => {
-    console.log("intento obtener el historial del id: " + req.body.songId)
+    //console.log("intento obtener el historial del id: " + req.body.songId)
     const { songId } = req.body
     try {
         if (!songId) {
@@ -13,13 +14,13 @@ export const artistSongsHistoryController = async (req: express.Request, res: ex
                 }
             })
         }
-        console.log("Sigo")
+        //console.log("Sigo")
         const songDAO = req.db!.createSongDAO()
         const artistDAO = req.db!.createArtistDAO()
         const genreDAO = req.db!.createGenreDAO()
         const song = await songDAO.findById(songId)
         const artist = await artistDAO.findByUid(req.uid!)
-        console.log("He encontrado al artista: " + artist?.username + " y la canción " + song?.title )
+        //console.log("He encontrado al artista: " + artist?.username + " y la canción " + song?.title + " con hisotrial: " + song?.versionHistory)
 
         if (!song) {
             return res.status(Number(apiErrorCodes[3001].httpCode)).json({
@@ -38,7 +39,8 @@ export const artistSongsHistoryController = async (req: express.Request, res: ex
                 }
             })
         }
-        console.log("Sigo he encontrado artista y cancion")
+        
+        //console.log("Sigo he encontrado artista y cancion")
         const history = await Promise.all(song.versionHistory!.map(async (version) => {
             const song = await songDAO.findById(version)
             const genres = await Promise.all(song!.genres.map(async (genre) => genreDAO.findById(genre)))
@@ -58,7 +60,26 @@ export const artistSongsHistoryController = async (req: express.Request, res: ex
                 }))
             }
         }))
-        console.log("Tiene un historial con: " + history.forEach((song) => song._id))
+        
+        const actualSong = await songDAO.findById(songId)
+        const actualGenres = await Promise.all(actualSong!.genres.map(async (genre) => genreDAO.findById(genre)))
+        const actualCollaborators = await Promise.all(song!.collaborators.map(async (collaborator) => await artistDAO.findById(collaborator.artist)))
+
+        history.push( {
+            ...actualSong,
+            productType: undefined,
+            author: undefined,
+            ratings: undefined,
+            songDir: undefined,
+            genres: actualGenres.map(genre => genre!.genre),
+            collaborators: actualCollaborators.map(collaborator => ({
+                _id: collaborator?._id,
+                artistUsername: collaborator?.artistUsername,
+                artistName: collaborator?.artistName,
+                artistImgUrl: collaborator?.artistImgUrl
+            }))
+        })
+
         return res.json({
             data: history
         })
